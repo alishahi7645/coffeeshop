0d606ad08a3422115199e32547aa144891ee814b
alishahi <shahy8227@gmail.com>
2025-01-15 21:59:03 +0330

    end code header , hero

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
+import { IoMdMenu } from "react-icons/io";
+import logomenu from "../../assets/img/why-2.png";
+import { IoMdClose } from "react-icons/io";
+
 
 function Header() {
+  const [isMenuOpen, setisMenuOpen] = useState(false);
+
+  const toggle = () => {
+    setisMenuOpen(!isMenuOpen);
+  };
   return (
-    <div>
-      <h1 className='bg-red-500'>hello</h1>
-    </div>
-  )
+    <header className="absolute top-0 left-0 w-full text-white z-10">
+      <nav className="px-10 py-5">
+        <div className="flex justify-between items-center">
+          <div className="hidden lg:flex ">
+            <h1>کافه خلیفه</h1>
+          </div>
+
+          <div className="lg:hidden">
+            <button onClick={toggle} className="lg:hidden">
+              <IoMdMenu className="w-6 h-6" />
+            </button>
+          </div>
+
+          <div className="hidden lg:flex gap-8">
+            <ul className="flex gap-8">
+              <li>خانه</li>
+              <li>منو</li>
+              <li>درباره ما</li>
+              <li>ارتباط با ما</li>
+            </ul>
+          </div>
+
+          <div className="flex">
+            <button className="mx-2 hidden bg-cream px-4 py-2 rounded-3xl text-gray-950 lg:block">
+              ثبت نام
+            </button>
+            <button className="hidden mx-2 lg:block">ورود</button>
+          </div>
+        </div>
+      </nav>
+
+      {/* منوی موبایل */}
+      <div
+        className={`fixed rela w-48 top-0 right-0 h-screen bg-gray-300 text-gray-950 p-4 transform transition-transform duration-300 ease-in-out ${
+          isMenuOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="">
+          <div className="w-full">
+            <div className="w-full flex justify-end">
+            <IoMdClose className="absolute top-0 left-0 bg-gray-700 text-white rounded-xl cursor-pointer" onClick={toggle}/>
+            </div>
+          </div>
+          <div className="w-full h-96 bg-gray-500 px-3 py-2 rounded-2xl text-center mt-10">
+            <div className="w-full bg-gray-500 px-3 py-2 rounded-2xl text-center">
+              <h1 className="text-red-900 font-bold">کافه خلیفه</h1>
+            </div>
+            <ul className="flex-col w-full bg-gray-500 mt-1 rounded-2xl p-2 gap-8">
+              <li className="w-full my-3 text-white hover:bg-gray-300 hover:text-gray-800 rounded-lg px-1 cursor-pointer transition-all duration-300">
+                خانه
+              </li>
+              <li className="w-full my-3 text-white hover:bg-gray-300 hover:text-gray-800 rounded-lg px-1 cursor-pointer transition-all duration-300">
+                منو
+              </li>
+              <li className="w-full my-3 text-white hover:bg-gray-300 hover:text-gray-800 rounded-lg px-1 cursor-pointer transition-all duration-300">
+                درباره ما
+              </li>
+              <li className="w-full my-3 text-white hover:bg-gray-300 hover:text-gray-800 rounded-lg px-1 cursor-pointer transition-all duration-300">
+                ارتباط با ما
+              </li>
+            </ul>
+            <div className="">
+              <div className="w-full mt-5">
+                <img src={logomenu} className="m-auto" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import imagehero from '../../assets/img/hero.jpg'
 
 function Hero() {
   return (
-    <div>
-      
+    <div style={{backgroundImage:`url(${imagehero})`}} className='w-full h-[100vh] bg-cover bg-no-repeat relative flex justify-center items-center' >
+      <div className="absolute backdrop-blur-sm top-2 px-3 lg:top-28 md:right-20 lg:backdrop-blur-0">
+        <div className="">
+            <h2 className='text-cream text-6xl my-10'>کافه خلیفه</h2>
+        </div>
+        <div className="">
+            <p className='text-xl text-gray-200 my-10 leading-9'>فراتر از یک کافه. جایی برای استراحت. آرامش و تجربه های جدید
+            <br />
+            به کافه ما خوش آمدید !    
+            </p>
+        </div>
+        <div className="">
+            <button className='mx-2 bg-cream px-4 rounded-3xl font-bold text-gray-950 py-2 shadow-sm shadow-cream'>ثبت سفارش</button>
+        </div>
+      </div>
     </div>
   )
 }
