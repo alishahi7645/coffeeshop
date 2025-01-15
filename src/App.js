a84ee61eecc1d4d24021a14ff69b7dc29daddec0
alishahi <shahy8227@gmail.com>
2025-01-15 14:54:13 +0330

    start code header , hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
 
 function App() {
   return (
     <div>
       <Header/>
+      <Heroo/>
     </div>
   )
 }
