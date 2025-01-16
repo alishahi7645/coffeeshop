69da035fe157c16587c44725321e999bc31e7d19
alishahi <shahy8227@gmail.com>
2025-01-16 12:52:16 +0330

    start code menu

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import coffeeMenu from '../../data'
+
+function Menu() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Menu
---
+++ src/data.js
@@ -0,0 +1,43 @@
+import image1 from "./assets/img/coffee-1.png"
+import image2 from "./assets/img/coffee-2.png"
+import image3 from "./assets/img/coffee-3.png"
+import image4 from "./assets/img/coffee-4.png"
+
+
+const coffeeMenu = [
+    {
+      id: 1,
+      name: "کاپوچینو کلاسیک",
+      image: image1,
+      price: 45000,
+      ingredients: ["اسپرسو", "شیر داغ"],
+      description: "ترکیبی متعادل از اسپرسو و شیر با لایه‌ای غلیظ از فوم شیر."
+    },
+    {
+      id: 2,
+      name: "لاته آرت",
+      image: image2,
+      price: 40000,
+      ingredients: ["اسپرسو", "شیر داغ"],
+      description: "لاته‌ای نرم با طراحی هنری روی فوم شیر، مناسب برای دوست‌داران قهوه ملایم."
+    },
+    {
+      id: 3,
+      name: "موکا شکلاتی",
+      image: image3,
+      price: 50000,
+      ingredients: ["اسپرسو", "شکلات داغ"],
+      description: "ترکیبی از اسپرسو و شکلات با خامه‌ای لذیذ، انتخابی عالی برای طرفداران طعم شیرین."
+    },
+    {
+      id: 4,
+      name: "آفوگاتو ایتالیایی",
+      image: image4,
+      price: 55000,
+      ingredients: ["اسپرسو", "بستنی وانیلی"],
+      description: "ترکیبی منحصر به فرد از اسپرسو داغ و بستنی سرد، یک دسر قهوه‌ای فوق‌العاده."
+    }
+  ];
+
+
+  export default coffeeMenu
